6be20d35c09796acf4decac0da052aa62e5ea39a
Ronalbis Altuve <corvencarespaldos@gmail.com>
2026-04-28 19:17:28 -0400

    Fix: next.config and port for Railway

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  // Configuración recomendada para despliegues en contenedores (como Railway)
-  output: "standalone",
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true }
-};
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true }
+}
 
-export default nextConfig;
+export default nextConfig
